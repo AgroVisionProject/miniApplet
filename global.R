ff9dd79ca9c2d44181767c0c8ca0148065039768
Elissa Chasen <emchasen@gmail.com>
2023-04-24 17:07:30 -0500

    reactive dataframe working

--- global.R
+++ global.R
@@ -20,7 +20,40 @@ sites <- read.csv("data/sampleSites.csv")
 fert = seq(from = 0, to = 300, by = 10)
 
 sims <-  readxl::read_xlsx("data/simulationNames.xlsx")
+sims  <- sims %>%
+  filter(simulation == 1 
+         | simulation == 2)
 simNames = sims$cropSystem
+
+# determine respose curve
+responseCurve <- function(dataframe, fun) {
+  
+  response <- c()
+  #constant
+  if(fun == 0) {
+    response = rep(dataframe$a, times = length(fert))
+  }
+  #non-negative quadratic
+  if(fun == 1) {
+    response = dataframe$a + (dataframe$b*fert) + (dataframe$c*fert^2)
+  }
+  # gompertz
+  if(fun == 3) {
+    response = dataframe$a * exp((-dataframe$b)*(dataframe$c^fert))
+  }
+  # piecewise linear
+  if(fun == 4) {
+    response = ifelse(fert < dataframe$c, dataframe$a + (dataframe$b*fert),
+                     dataframe$a + (dataframe$b*dataframe$c))
+  }
+  #linear
+  if(fun == 5) {
+    response = dataframe$a + (dataframe$b*fert)
+  }
+  
+  return(response)
+  
+}
 
 # define equations
 # const = rep(cons_ex1$a, times = length(fert))
--- server.R
+++ server.R
@@ -21,27 +21,11 @@ server <- function(input, output, session) {
       addProviderTiles("Esri.WorldTopoMap")
   }
   
-  # make_df <- function(dat) {
-  #   dat %>%
-  #     filter(sim == input$simName) %>%
-  #     filter(lat == sampleSite$lat,
-  #            lon == sampleSite$lng)
-  # }
-  # 
-  ##TODO is sampleSite a function? It needs to be reactive to input$map_marker_click
-  
   
   # reactiveVal for the map object, and corresponding output object.
   react_map <- reactiveVal(base_map())
   
   # reactiveVals for map---------------------------
-  
-  ## filter agroIbis output to county of interest---------------------
-  # selected_county <- reactive({
-  #   p <- input$map_shape_click
-  #   map_df %>% filter(STATE_NAME == stateName) %>% filter(NAME == p$id)
-  #   
-  # })
   
   # map-------------------------
   output$map <- renderLeaflet({
@@ -74,305 +58,139 @@ server <- function(input, output, session) {
     }
     
   })
+
+  ##TODO is sampleSite a function? It needs to be reactive to input$map_marker_click
+  
+  selectedPoint <- reactiveVal()
   
   observeEvent(input$map_marker_click, {
     
     
-    map_click_info <- input$map_marker_click
-    print("marker click info")
-    print(map_click_info)
-    print(sim())
+    click <- input$map_marker_click
+    lat <- click$lat
+    lon <- click$lng
     
-    # does this zoom into site?
+    selectedPoint(
+      list(lat = lat, lon = lon)
+    )
+
+    # zoom to site
     leafletProxy("map") %>%
-      setView(lng = map_click_info$lng, lat = map_click_info$lat, zoom = 11) %>%
-      addMarkers(lng = map_click_info$lng, lat = map_click_info$lat)
+      setView(lng = lon, lat = lat, zoom = 11) %>%
+      clearGroup("sampleSite") %>%
+      addMarkers(lng = lon, lat = lat, group = "sampleSite")
     
-    # 
-    # ##TODO this needs to be reactive to the input$simName, as well as the mapclickinfo
-    # # does this df need to be a reactive df?
-    # # yield_df_sub <- eventReactive(button, { })
-    # # yield_df_sub <- yield_df1 %>%
-    # #   filter(lat == map_click_info$lat,
-    # #          lon == map_click_info$lng)
-    # yielddf <- yield_df_sub()
-    # #print('yield df')
-    # # print(yield_df_sub)
-    # 
-    # ##TODO this needs to be reactive to the input$simName, as well as the mapclickinfo
-    # # leach_df_sub <- eventReactive(button, { })
-    # # leach_df_sub <- leach_df1 %>%
-    # #   filter(lat == map_click_info$lat,
-    # #          lon == map_click_info$lng)
-    # # print('leach df')
-    # # print(leach_df_sub)
-    # leach_df <- leach_df_sub()
-    # 
-    # yield_y <- c()
-    # if(yield_df$fun == 0) {
-    #   yield_y = rep(yield_df$a, times = length(fert))
-    # }
-    # if(yield_df$fun == 3) {
-    #   yield_y = yield_df$a * exp((-yield_df$b)*(yield_df$c^fert))
-    # }
-    # if(yield_df$fun == 4) {
-    #   yield_y = ifelse(fert < yield_df$c, yield_df$a + (yield_df$b*fert),
-    #                    yield_df$a + (yield_df$b*yield_df$c))
-    # }
-    # if(yield_df$fun == 5) {
-    #   yield_y = yield_df$a + (yield_df$b*fert)
-    # }
-    # #print(yield_y)
-    # 
-    # leach_y <- c()
-    # if(leach_df$fun == 0) {
-    #   leach_y = rep(leach_df$a, times = length(fert))
-    # }
-    # if(leach_df$fun == 1) {
-    #   leach_y = leach_df$a + (leach_df$b*fert) + (leach_df$c*fert^2)
-    # }
-    # #print(leach_y)
-    # 
-    # ##TODO should this dataframe be reactive?
-    # df = data.frame(fert = fert, yield = yield_y, leaching = leach_y)
-    # print(df)
-    # 
-    # 
-    # output$plot1 <- renderPlotly({
-    #   
-    #   # validate(
-    #   #   need(df, "select new location")
-    #   # )
-    #   #
-    #   # if(is.null(df)) {
-    #   #   output$plot1 <- NULL
-    #   # }
-    #   
-    #   #output$plotDone <- renderUI({tags$input(type="hidden", value="TRUE")})
-    #   
-    #   plot_ly(df, x = ~fert, y = ~ yield, name = "Yield (bu/ac)",
-    #           type = 'scatter', mode = 'lines',
-    #           line = list(color = "#5dbb63", width = 4),
-    #           hovertext = ~ round(yield, 2),
-    #           hoverinfo = "text") %>%
-    #     add_trace(y = ~ leaching, name = "Nitrate leaching (lb/ac)",
-    #               line = list(color = "#c99f6e", width = 4),
-    #               hovertext = ~ round(leaching, 2),
-    #               hoverinfo = "text") %>%
-    #     layout(title = "Corn yield and nitrate leaching response \n to N fertilizer \n",
-    #            xaxis = list(title = "N fertilizer (N lb/ac)"),
-    #            yaxis = list (title = " "),
-    #            hovermode = "x unified")
-    #   
-    # })
-    # 
-    # output$range <- renderUI({
-    #   
-    #   sliderInput(
-    #     inputId = "fertRange",
-    #     label = "N fertilizer (kg/ha)",
-    #     min = 0, max = 300, value = 100, step = 10
-    #   )
-    # })
-    # 
-    # output$values <- render_gt({
-    #   
-    #   Nval <- unique(input$fertRange)
-    #   
-    #   df %>%
-    #     filter(fert == Nval) %>%
-    #     mutate(leaching = round(leaching, 2),
-    #            yield = round(yield, 2)) %>%
-    #     gt() %>%
-    #     cols_label(
-    #       fert = "N fertilizer (kg/ha)",
-    #       yield = "Yield (bu/ac)",
-    #       leaching = "Nitrate leaching (kg/ha)"
-    #     )
-    # })
   })
   
-  
-  # determine which sim number
-  sim <- reactive({
+  observeEvent(selectedPoint(), {
 
-    filter(sims, cropSystem == input$simName)$simulation
+    print(selectedPoint())
 
   })
   
-  #print(sim())
+
+  df <- reactive({
+    
+    req(selectedPoint())
+    req(input$simName)
+    
+    simNumber <- filter(sims, cropSystem == input$simName)
+    selected_sim <- simNumber$simulation
+    #print(paste("sim:", selected_sim))
+    site_lat <- selectedPoint()$lat
+    #print(paste("lat:", site_lat))
+    site_lon <- selectedPoint()$lon
+    #print(paste("lon:", site_lon))
+    
+    yield_df_sum <- yield_df %>%
+      filter(sim == selected_sim,
+             lat == site_lat,
+             lon == site_lon)
+    #print(yield_df_sum)
+    
+    leach_df_sum <- leach_df %>%
+      filter(sim == selected_sim,
+             lat == site_lat,
+             lon == site_lon)
+    #print(leach_df_sum)
+    
+    yieldFun <- yield_df_sum$fun
+    leachFun <- leach_df_sum$fun
+    
+    yield_y <- responseCurve(dataframe = yield_df_sum, fun = yieldFun)
+    leach_y <- responseCurve(dataframe = leach_df_sum, fun = leachFun)
+    
+    data.frame(fert = fert, yield = yield_y, leaching = leach_y)
+    
+  })
+                  
+  observeEvent(input$plot, {
+
+    output$plot1 <- renderPlotly({
+
+      # validate(
+      #   need(df, "select new location")
+      # )
+      #
+      # if(is.null(df)) {
+      #   output$plot1 <- NULL
+      # }
+
+      dat <- df()
+      head(dat)
+
+      #output$plotDone <- renderUI({tags$input(type="hidden", value="TRUE")})
+
+      plot_ly(dat, x = ~fert, y = ~ yield, name = "Yield (bu/ac)",
+              type = 'scatter', mode = 'lines',
+              line = list(color = "#5dbb63", width = 4),
+              hovertext = ~ round(yield, 2),
+              hoverinfo = "text") %>%
+        add_trace(y = ~ leaching, name = "Nitrate leaching (lb/ac)",
+                  line = list(color = "#c99f6e", width = 4),
+                  hovertext = ~ round(leaching, 2),
+                  hoverinfo = "text") %>%
+        layout(title = "Corn yield and nitrate leaching response \n to N fertilizer \n",
+               xaxis = list(title = "N fertilizer (N lb/ac)"),
+               yaxis = list (title = " "),
+               hovermode = "x unified")
+
+    })
+  })
+
+
+      output$range <- renderUI({
+
+        sliderInput(
+          inputId = "fertRange",
+          label = "N fertilizer (kg/ha)",
+          min = 0, max = 300, value = 100, step = 10
+        )
+      })
+
+      output$values <- render_gt({
+
+        Nval <- unique(input$fertRange)
+
+        df %>%
+          filter(fert == Nval) %>%
+          mutate(leaching = round(leaching, 2),
+                 yield = round(yield, 2)) %>%
+          gt() %>%
+          cols_label(
+            fert = "N fertilizer (kg/ha)",
+            yield = "Yield (bu/ac)",
+            leaching = "Nitrate leaching (kg/ha)"
+          )
+      })
   
-  # plot_lat <- eventReactive(input$map_marker_click, {
-  #   
-  #   map_click_info <- input$map_marker_click
-  #   map_click_info$lat
-  #   
-  # })
-  # 
-  # print(plot_lat())
-  # 
-  # plot_lon <- eventReactive(input$map_marker_click, {
-  #   
-  #   map_click_info <- input$map_marker_click
-  #   map_click_info$lng
-  #   
-  # })
-  # 
-  # print(plot_lon())
-  
-#   yield_df_sub <- eventReactive(input$plot, {
-#     
-#     #print(sim())
-#     #print(plot_lat())
-#     
-#     yield_df %>% filter(sim == sim()) %>% 
-#       filter(lat == plot_lat(),
-#              lon == plot_lon())
-# 
-#   })
-#   
-#   print(yield_df_sub())
-#   
-#   leach_df_sub <- eventReactive(input$plot, {
-#     
-#     leach_df %>% filter(sim == sim()) %>% 
-#       filter(lat == plot_lat(),
-#              lon == plot_lon())
-#     
-#   })
-#   
-#   print(leach_df_sub())
-#   
-# }
-#   
-  # observeEvent(input$map_marker_click, {
-  # 
-  # 
-  #   map_click_info <- input$map_marker_click
-  #   #print("marker click info")
-  #   #print(map_click_info)
-  # 
-  #   # does this zoom into site?
-  #   leafletProxy("map") %>%
-  #     setView(lng = map_click_info$lng, lat = map_click_info$lat, zoom = 11) %>%
-  #     addMarkers(lng = map_click_info$lng, lat = map_click_info$lat)
-  # 
-  #   ID = map_click_info$id
-  #   print(paste("line 119", ID))
-  # 
-  #   ##TODO this needs to be reactive to the input$simName, as well as the mapclickinfo
-  #   # does this df need to be a reactive df?
-  #   # yield_df_sub <- eventReactive(button, { })
-  #   # yield_df_sub <- yield_df1 %>%
-  #   #   filter(lat == map_click_info$lat,
-  #   #          lon == map_click_info$lng)
-  #   yielddf <- yield_df_sub()
-  #   #print('yield df')
-  #   # print(yield_df_sub)
-  # 
-  #   ##TODO this needs to be reactive to the input$simName, as well as the mapclickinfo
-  #   # leach_df_sub <- eventReactive(button, { })
-  #   # leach_df_sub <- leach_df1 %>%
-  #   #   filter(lat == map_click_info$lat,
-  #   #          lon == map_click_info$lng)
-  #   # print('leach df')
-  #   # print(leach_df_sub)
-  #   leach_df <- leach_df_sub()
-  # 
-  #   yield_y <- c()
-  #   if(yield_df$fun == 0) {
-  #     yield_y = rep(yield_df$a, times = length(fert))
-  #   }
-  #   if(yield_df$fun == 3) {
-  #     yield_y = yield_df$a * exp((-yield_df$b)*(yield_df$c^fert))
-  #   }
-  #   if(yield_df$fun == 4) {
-  #     yield_y = ifelse(fert < yield_df$c, yield_df$a + (yield_df$b*fert),
-  #                      yield_df$a + (yield_df$b*yield_df$c))
-  #   }
-  #   if(yield_df$fun == 5) {
-  #     yield_y = yield_df$a + (yield_df$b*fert)
-  #   }
-  #   #print(yield_y)
-  # 
-  #   leach_y <- c()
-  #   if(leach_df$fun == 0) {
-  #     leach_y = rep(leach_df$a, times = length(fert))
-  #   }
-  #   if(leach_df$fun == 1) {
-  #     leach_y = leach_df$a + (leach_df$b*fert) + (leach_df$c*fert^2)
-  #   }
-  #   #print(leach_y)
-  # 
-  #   ##TODO should this dataframe be reactive?
-  #   df = data.frame(fert = fert, yield = yield_y, leaching = leach_y)
-  #   print(df)
-  # 
-  # 
-  #     output$plot1 <- renderPlotly({
-  # 
-  #       # validate(
-  #       #   need(df, "select new location")
-  #       # )
-  #       #
-  #       # if(is.null(df)) {
-  #       #   output$plot1 <- NULL
-  #       # }
-  # 
-  #       #output$plotDone <- renderUI({tags$input(type="hidden", value="TRUE")})
-  # 
-  #       plot_ly(df, x = ~fert, y = ~ yield, name = "Yield (bu/ac)",
-  #               type = 'scatter', mode = 'lines',
-  #               line = list(color = "#5dbb63", width = 4),
-  #               hovertext = ~ round(yield, 2),
-  #               hoverinfo = "text") %>%
-  #         add_trace(y = ~ leaching, name = "Nitrate leaching (lb/ac)",
-  #                   line = list(color = "#c99f6e", width = 4),
-  #                   hovertext = ~ round(leaching, 2),
-  #                   hoverinfo = "text") %>%
-  #         layout(title = "Corn yield and nitrate leaching response \n to N fertilizer \n",
-  #                xaxis = list(title = "N fertilizer (N lb/ac)"),
-  #                yaxis = list (title = " "),
-  #                hovermode = "x unified")
-  # 
-  #     })
-  # 
-  #     output$range <- renderUI({
-  # 
-  #       sliderInput(
-  #         inputId = "fertRange",
-  #         label = "N fertilizer (kg/ha)",
-  #         min = 0, max = 300, value = 100, step = 10
-  #       )
-  #     })
-  # 
-  #     output$values <- render_gt({
-  # 
-  #       Nval <- unique(input$fertRange)
-  # 
-  #       df %>%
-  #         filter(fert == Nval) %>%
-  #         mutate(leaching = round(leaching, 2),
-  #                yield = round(yield, 2)) %>%
-  #         gt() %>%
-  #         cols_label(
-  #           fert = "N fertilizer (kg/ha)",
-  #           yield = "Yield (bu/ac)",
-  #           leaching = "Nitrate leaching (kg/ha)"
-  #         )
-  #     })
-  # })
-#   
-#   
-#   
-#   
+
+
   observeEvent(input$reset, {
     # reset the map
     react_map(base_map())
-    # reset the click counts
-    #clicks$count <- 0
-    #output$plot1 <- NULL
-    #df <- NULL
+    output$plot1 <- NULL
+    df <- NULL
 
   })
 
